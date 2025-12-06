c06fd0352778e055937bb14f481a3a185732f152
BulqeyisMammadova <bulgeyisrm-azmpa201@code.edu.az>
2025-12-06 20:48:40 +0400

    axios

--- assets/scripts/detail.js
+++ assets/scripts/detail.js
@@ -2,24 +2,50 @@ const params = new URLSearchParams(document.location.search)
 const id = params.get('id')
 const detail = document.getElementById("detail")
 
-fetch(`https://api.tvmaze.com/shows/${id}`).then(res => res.json()).then(data => {
-    console.log(data);
+// fetch(`https://api.tvmaze.com/shows/${id}`).then(res => res.json()).then(data => {
+//     console.log(data);
 
-    detail.innerHTML = `
+//     detail.innerHTML = `
+//       <div class="detailImg">
+//         <img src="${data.image.medium}" alt="${data.name}">
+//       </div>
+//       <div class="detailText">
+//         <h2>${data.name}</h2>
+//         <p><span class="detailTitle">Type:</span>${data.type}</p>
+//         <p><span class="detailTitle">Language:</span>${data.language}</p>
+//         <p><span class="detailTitle">Genres:</span>${data.genres.join(', ')}</p>
+//         <p><span class="detailTitle">Status:</span>${data.status}</p>
+//         <p><span class="detailTitle">Runtime:</span>${data.runtime}</p>
+//         <p><span class="detailTitle">Premiered:</span>${data.premiered}</p>
+//         <p><span class="detailTitle">Rating:</span>${data.rating.average}</p>
+//         <p><span class="detailTitle">Country:</span>${data.network.country?.name}</p>
+//         <div class="summary"><p ><span class="detailTitle">Summary:</span>${data.summary}</p>
+//         </div>
+        
+        
+        
+        
+//       </div>
+//     `
+// })
+
+
+axios.get(`https://api.tvmaze.com/shows/${id}`).then(res=>{
+  detail.innerHTML = `
       <div class="detailImg">
-        <img src="${data.image.medium}" alt="${data.name}">
+        <img src="${res.data.image.medium}" alt="${res.data.name}">
       </div>
       <div class="detailText">
-        <h2>${data.name}</h2>
-        <p><span class="detailTitle">Type:</span>${data.type}</p>
-        <p><span class="detailTitle">Language:</span>${data.language}</p>
-        <p><span class="detailTitle">Genres:</span>${data.genres.join(', ')}</p>
-        <p><span class="detailTitle">Status:</span>${data.status}</p>
-        <p><span class="detailTitle">Runtime:</span>${data.runtime}</p>
-        <p><span class="detailTitle">Premiered:</span>${data.premiered}</p>
-        <p><span class="detailTitle">Rating:</span>${data.rating.average}</p>
-        <p><span class="detailTitle">Country:</span>${data.network.country?.name}</p>
-        <div class="summary"><p ><span class="detailTitle">Summary:</span>${data.summary}</p>
+        <h2>${res.data.name}</h2>
+        <p><span class="detailTitle">Type:</span>${res.data.type}</p>
+        <p><span class="detailTitle">Language:</span>${res.data.language}</p>
+        <p><span class="detailTitle">Genres:</span>${res.data.genres.join(', ')}</p>
+        <p><span class="detailTitle">Status:</span>${res.data.status}</p>
+        <p><span class="detailTitle">Runtime:</span>${res.data.runtime}</p>
+        <p><span class="detailTitle">Premiered:</span>${res.data.premiered}</p>
+        <p><span class="detailTitle">Rating:</span>${res.data.rating.average}</p>
+        <p><span class="detailTitle">Country:</span>${res.data.network.country?.name}</p>
+        <div class="summary"><p ><span class="detailTitle">Summary:</span>${res.data.summary}</p>
         </div>
         
         
@@ -27,4 +53,6 @@ fetch(`https://api.tvmaze.com/shows/${id}`).then(res => res.json()).then(data =>
         
       </div>
     `
+}).catch(err=>{
+  console.error(err)
 })
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,83 +13,158 @@ hamburger.addEventListener('click', () => {
 let filterTypeFilms = [];
 
 
-fetch("https://api.tvmaze.com/shows").then(res => res.json()).then(data => {
+// fetch("https://api.tvmaze.com/shows").then(res => res.json()).then(data => {
 
-    data.forEach(film => {
-        if (!filterTypeFilms.includes(film.type)) {
-            filterTypeFilms.push(film.type);
-        }
-    });
+//     data.forEach(film => {
+//         if (!filterTypeFilms.includes(film.type)) {
+//             filterTypeFilms.push(film.type);
+//         }
+//     });
 
-    for (let i = 0; i < filterTypeFilms.length; i++) {
-        const cards = document.createElement("div");
-        cards.innerHTML = `
-            <h4 class="filmText">${filterTypeFilms[i]}</h4>
-            <div class="imgTitle"></div>
-        `;
+//     for (let i = 0; i < filterTypeFilms.length; i++) {
+//         const cards = document.createElement("div");
+//         cards.innerHTML = `
+//             <h4 class="filmText">${filterTypeFilms[i]}</h4>
+//             <div class="imgTitle"></div>
+//         `;
 
-        filmType.appendChild(cards);
+//         filmType.appendChild(cards);
 
-        const titleImg = cards.querySelector(".imgTitle");
+//         const titleImg = cards.querySelector(".imgTitle");
 
-        const typeOfFilm = data.filter(item => item.type == filterTypeFilms[i]);
+//         const typeOfFilm = data.filter(item => item.type == filterTypeFilms[i]);
 
 
-        let start = 0;
-        let end = 9;
+//         let start = 0;
+//         let end = 9;
 
-        typeOfFilm.slice(start, end).forEach(film => {
-            titleImg.innerHTML += `<a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>
+//         typeOfFilm.slice(start, end).forEach(film => {
+//             titleImg.innerHTML += `<a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>
                 
-            `;
-        });
+//             `;
+//         });
 
 
-        if (typeOfFilm.length > end) {
-            const addBtn = document.createElement("button")
-            addBtn.textContent = "+9 Added Films"
-            cards.appendChild(addBtn)
-            addBtn.addEventListener("click", ()=>{
-                typeOfFilm.slice(end, end +9).forEach(film => {
-                    titleImg.innerHTML += `<img src="${film.image.medium}" alt="">`
-                });
-                end +=9
-            })
-        }
+//         if (typeOfFilm.length > end) {
+//             const addBtn = document.createElement("button")
+//             addBtn.textContent = "+9 Added Films"
+//             cards.appendChild(addBtn)
+//             addBtn.addEventListener("click", ()=>{
+//                 typeOfFilm.slice(end, end +9).forEach(film => {
+//                     titleImg.innerHTML += `<a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>
+//                 });
+//                 end +=9
+//             })
+//         }
 
         
 
-        search.addEventListener("input", (e) => {
-            const currentVal = e.target.value.toLowerCase();
+//         search.addEventListener("input", (e) => {
+//             const currentVal = e.target.value.toLowerCase();
                    
-            mainSection.style.display = currentVal == "" ? "block" : "none"
+//             mainSection.style.display = currentVal == "" ? "block" : "none"
             
-            serachSection.innerHTML = ""
-            const filterData = data.filter(film => film.name.toLowerCase().includes(currentVal))
-            if(filterData.length == 0){
-                const notFound = document.createElement("div");
-                notFound.textContent = "Not Found Films"
-                notFound.classList.add("notFound")
-                serachSection.appendChild(notFound)
-            }else{
-                filterData.forEach(film=>{
-                    const cards = document.createElement("div")
-                   cards.innerHTML += `<div class="card">
-                   <a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>
-                    <div>
-                    <h2 class="searchName">${film.name}</h2>
-                    </div>
-                   </div>`
-                   serachSection.appendChild(cards)
-                })
-            }           
-        });
+//             serachSection.innerHTML = ""
+//             const filterData = data.filter(film => film.name.toLowerCase().includes(currentVal))
+//             if(filterData.length == 0){
+//                 const notFound = document.createElement("div");
+//                 notFound.textContent = "Not Found Films"
+//                 notFound.classList.add("notFound")
+//                 serachSection.appendChild(notFound)
+//             }else{
+//                 filterData.forEach(film=>{
+//                     const cards = document.createElement("div")
+//                    cards.innerHTML += `<div class="card">
+//                    <a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>`
+//                     <div>
+//                     <h2 class="searchName">${film.name}</h2>
+//                     </div>
+//                    </div>`
+//                    serachSection.appendChild(cards)
+//                 })
+//             }           
+//         });
         
-    }        
-});
+//     }        
+// }).catch(error => console.error(error));
 
 
 
 
 
 
+axios.get('https://api.tvmaze.com/shows')
+      .then(res => {
+        res.data.forEach(film => {
+            if (!filterTypeFilms.includes(film.type)) {
+                filterTypeFilms.push(film.type);
+            }
+            
+        });
+        for (let i = 0; i < filterTypeFilms.length; i++) {
+            const cards = document.createElement("div");
+            cards.innerHTML = `
+                <h4 class="filmText">${filterTypeFilms[i]}</h4>
+                <div class="imgTitle"></div>
+            `;
+    
+            filmType.appendChild(cards);
+    
+            const titleImg = cards.querySelector(".imgTitle");
+    
+            const typeOfFilm = res.data.filter(item => item.type == filterTypeFilms[i]);
+    
+    
+            let start = 0;
+            let end = 9;
+    
+            typeOfFilm.slice(start, end).forEach(film => {
+                titleImg.innerHTML += `<a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>
+                    
+                `;
+            });
+    
+    
+            if (typeOfFilm.length > end) {
+                const addBtn = document.createElement("button")
+                addBtn.textContent = "+9 Added Films"
+                cards.appendChild(addBtn)
+                addBtn.addEventListener("click", ()=>{
+                    typeOfFilm.slice(end, end +9).forEach(film => {
+                        titleImg.innerHTML += `<a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>`
+                    });
+                    end +=9
+                })
+            }
+    
+            
+    
+            search.addEventListener("input", (e) => {
+                const currentVal = e.target.value.toLowerCase();
+                       
+                mainSection.style.display = currentVal == "" ? "block" : "none"
+                
+                serachSection.innerHTML = ""
+                const filterData = res.data.filter(film => film.name.toLowerCase().includes(currentVal))
+                if(filterData.length == 0){
+                    const notFound = document.createElement("div");
+                    notFound.textContent = "Not Found Films"
+                    notFound.classList.add("notFound")
+                    serachSection.appendChild(notFound)
+                }else{
+                    filterData.forEach(film=>{
+                        const cards = document.createElement("div")
+                       cards.innerHTML += `<div class="card">
+                       <a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>
+                        <div>
+                        <h2 class="searchName">${film.name}</h2>
+                        </div>
+                       </div>`
+                       serachSection.appendChild(cards)
+                    })
+                }           
+            });
+            
+        }        
+      }).catch(error => console.error(error));
+      
