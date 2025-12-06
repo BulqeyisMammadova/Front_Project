387212a56261ec849db4689a8405de8c202bc3ce
BulqeyisMammadova <bulgeyisrm-azmpa201@code.edu.az>
2025-12-07 00:37:49 +0400

    NavbarFilmGenres

--- assets/scripts/category.js
+++ assets/scripts/category.js
@@ -5,8 +5,24 @@ console.log(genres);
 const cards = document.querySelector(".cards")
 const title = document.querySelector(".title")
 
-fetch("https://api.tvmaze.com/shows").then(res=>res.json()).then(data=>{
-   const filterData = data.filter(film=> film.genres.includes(genres))
+// fetch("https://api.tvmaze.com/shows").then(res=>res.json()).then(data=>{
+//    const filterData = data.filter(film=> film.genres.includes(genres))
+//    console.log(filterData);
+//    title.innerHTML = genres
+
+//    filterData.forEach(film => {
+//     cards.innerHTML += `
+//                 <div class="card">
+//                     <a href="/pages/detail.html?id=${film.id}"><img  src="${film.image.medium}" alt=""></a>
+//                     <h4 class="filmText">${film.name}</h4>
+//                 </div>
+//     `
+//    });
+// })
+
+
+axios.get('https://api.tvmaze.com/shows').then(res=>{
+    const filterData = res.data.filter(film=> film.genres.includes(genres))
    console.log(filterData);
    title.innerHTML = genres
 
